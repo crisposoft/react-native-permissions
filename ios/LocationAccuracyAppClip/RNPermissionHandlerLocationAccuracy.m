ad5da4667127b147343988ff0cd796c4a02ee17e
Telechi Nicolae <tux2nicolae@gmail.com>
2023-09-17 13:58:45 +0300

    Add separate implementation for AppClip location pods

---
+++ ios/LocationAccuracyAppClip/RNPermissionHandlerLocationAccuracy.m
@@ -0,0 +1,35 @@
+#import "RNPermissionHandlerLocationAccuracy.h"
+
+@import CoreLocation;
+
+@interface RNPermissionHandlerLocationAccuracy() <CLLocationManagerDelegate>
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) void (^resolve)(RNPermissionStatus status);
+@property (nonatomic, strong) void (^reject)(NSError *error);
+
+@end
+
+@implementation RNPermissionHandlerLocationAccuracy
+
++ (NSArray<NSString *> * _Nonnull)usageDescriptionKeys {
+  return @[@"NSLocationTemporaryUsageDescriptionDictionary"];
+}
+
++ (NSString * _Nonnull)handlerUniqueId {
+  return @"ios.permission.LOCATION_ACCURACY";
+}
+
+- (void)checkWithResolver:(RCTPromiseResolveBlock _Nonnull)resolve
+                 rejecter:(RCTPromiseRejectBlock _Nonnull)reject {
+  reject(@"cannot_check_location_accuracy", @"Not available in appclip", nil);
+}
+
+- (void)requestWithPurposeKey:(NSString * _Nonnull)purposeKey
+                     resolver:(RCTPromiseResolveBlock _Nonnull)resolve
+                     rejecter:(RCTPromiseRejectBlock _Nonnull)reject {
+
+  return reject(@"cannot_request_location_accuracy", @"Not available in appclip", nil);
+}
+
+@end
